890f43af39f63ffd92100f5239e26d39b37b2d56
fiba-dev <fibarrola06@gmail.com>
2021-06-12 16:59:47 -0300

    last commit

--- header/index.js
+++ header/index.js
@@ -4,22 +4,22 @@ function headerComponent(el){
     <div class="ventana"> 
         <button class="boton__cerrar">X</button>
         <div class="ventana__vinculos"> 
-            <a href="../portfolio/portfolio.html" class="ventana__label">Portfolio</a>
-            <a href="#servicios"class="ventana__label">Servicios</a>
-            <a href="../escribime/contacto.html"class="ventana__label">Escribime</a></div>
+            <a href="https://fiba-dev.github.io/desafio-final-m4/portfolio/portfolio.html" class="ventana__label">Portfolio</a>
+            <a href="https://fiba-dev.github.io/desafio-final-m4/index.html#servicios"class="ventana__label">Servicios</a>
+            <a href="https://fiba-dev.github.io/desafio-final-m4/escribime/contacto.html"class="ventana__label">Escribime</a></div>
       
       </div>
  <header class="header" >  
  <div class="inicio">
-<a href="../index.html"> <img src="./imagenes/icon.jpg" class="header__logo"></a>
+<a href="../index.html"> <img src="http://images.ctfassets.net/0q692q1cfe6h/171oK9iedio26xXz2Y5mtg/0b666c72132118f9ba8f79dbaff726fa/icon.jpg" class="header__logo"></a>
 <a href="../index.html" class="header__incio">Inicio</a>
 </div>
  
    <div class="header__vinculos"> 
      <button class="header__menu-button">Menu</button>
-     <a href="./portfolio/portfolio.html" class="header__label">Portfolio</a>
-     <a href="./escribime/contacto.html"class="header__label">Escribime</a>
-     <a href="#servicios"class="header__label">Servicios</a>
+     <a href="https://fiba-dev.github.io/desafio-final-m4/portfolio/portfolio.html" class="header__label">Portfolio</a>
+     <a href="https://fiba-dev.github.io/desafio-final-m4/escribime/contacto.html"class="header__label">Escribime</a>
+     <a href="https://fiba-dev.github.io/desafio-final-m4/index.html#servicios"class="header__label">Servicios</a>
     </div>
  
 
